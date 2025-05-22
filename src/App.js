cfcdb635127787dfdbdae9d3882d7c5ed6f4ea46
galileodev12 <ri.santos@hotmail.com>
2025-05-21 22:11:02 -0300

    aula 8 -

--- src/App.js
+++ src/App.js
@@ -5,52 +5,52 @@ import react from "react";
 
 function App() {
 
-const [tarefas , setTarefas] = useState([]);
-const[input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([]);
+  const [input, setInput] = useState('');
 
 
 
-function handleInputTarefa(param){
-  setInput(param);
-}
+  function handleInputTarefa(param) {
+    setInput(param);
+  }
 
 
-function handleRegister(event){
-  event.preventDefault();
-  alert('cadastrando suas tarefas');
-  
-  setTarefas([
-    ...tarefas,input
-  ])
+  function handleRegister(event) {
+    event.preventDefault();
+    alert('cadastrando suas tarefas');
 
-  setInput('');
-}
+    setTarefas([
+      ...tarefas, input
+    ])
+
+    setInput('');
+  }
 
   return (
     <div>
-     <form onSubmit={handleRegister}>
-     <h1>Listas de tarefas</h1>
-    <label>Tarefa:</label><br/>
-    <input type="text" value={input}  placeholder="Digite sua tarefa"
-      onChange={(e) => handleInputTarefa(e.target.value)} /> <br/>
+      <form onSubmit={handleRegister}>
+        <h1>Listas de tarefas</h1>
+        <label>Tarefa:</label><br />
+        <input type="text" value={input} placeholder="Digite sua tarefa"
+          onChange={(e) => handleInputTarefa(e.target.value)} /> <br />
 
-    <button type="submit">
-      Cadastrar
-    </button> 
-    <br/>
-    
-      <ul>
-        { tarefas.map(tarefa => (
-          <li key={tarefa}> {tarefa} </li>
-        ))}
-      </ul>
+        <button type="submit">
+          Cadastrar
+        </button>
+        <br />
+
+        <ul>
+          {tarefas.map(tarefa => (
+            <li key={tarefa}> {tarefa} </li>
+          ))}
+        </ul>
 
 
-     </form>
-     
+      </form>
+
     </div>
-    
-    
+
+
   );
 }
 
